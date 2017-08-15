adf3fddb50e7ec50e132667a0a15315f0dbd0527
daniel kafer <daniel.j.kafer@gmail.com>
2017-08-15 16:52:49 -0500

    created preview component for archive list

---
+++ src/components/Preview.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="component preview">
+		<h2 class="subtitle">{{ title }}</h2>
+		<span class="date">{{ date }}</span>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'preview',
+		data () {
+			return {
+				title: 'Example Title',
+				date: '9/9/99'
+			}
+		}
+	}
+</script>
