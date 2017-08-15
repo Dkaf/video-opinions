74e88526eab8e2f2b6b9ea892d4c761dab537356
daniel kafer <daniel.j.kafer@gmail.com>
2017-08-15 16:53:27 -0500

    created simple component for youtube reviews

---
+++ src/components/Youtube.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="component youtube-article">
+		<iframe width="854" height="480" src="https://www.youtube.com/embed/N2VwIfi6LoY" frameborder="0" allowfullscreen></iframe>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'youtube'
+	}
+</script>
