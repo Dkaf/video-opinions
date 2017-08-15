2580610b54cff9ba91c2a4746e74d6f8a5b82e52
daniel kafer <daniel.j.kafer@gmail.com>
2017-08-15 16:51:40 -0500

    display preview of reviews in archive

--- src/components/Archive.vue
+++ src/components/Archive.vue
@@ -1,13 +1,20 @@
 <template>
   <div class="archive">
 	  <h1 class="title">Archive</h1>
+		<preview></preview>
   </div>
 </template>
 
 <script>
+import Preview from './Preview'
+
 export default {
-	name: 'archive'
-}
+	name: 'archive',
+	components: {
+		Preview
+	}
+} 
+
 </script>
 
 <style>
