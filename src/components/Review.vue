a730855ceab0240869df1a6e59a32ede9d91fda8
daniel kafer <daniel.j.kafer@gmail.com>
2017-08-15 16:53:11 -0500

    created review component to display reviews

---
+++ src/components/Review.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="component article">
+		<h1 class="title">{{ title }}</h1>
+		<span class="date">{{ date }}</span>
+		<div class="container">
+			<youtube></youtube>
+			<movie></movie>		
+		</div>
+	</div>
+</template>
+
+<script>
+import Youtube from './Youtube'
+import Movie from './Movie'
+
+export default {
+  name: 'article',
+  data () {
+	  return {
+		 title: "placeholder",
+		 date: "8/14/17"
+	  }
+  },
+  components: {
+	  Youtube,
+	  Movie
+  }
+}
+</script>
