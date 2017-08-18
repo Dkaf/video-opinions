ef671d0aa353655b8a0c78e2926e4105daa9c814
daniel kafer <daniel.j.kafer@gmail.com>
2017-08-18 16:53:45 -0500

    created a header component

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="component main-header">
+		<navigation></navigation>
+	</div>
+</template>
+
+<script>
+	import Navigation from "./Navigation"
+	export default {
+		name: 'main-header',
+		components: { Navigation }
+	}
+</script>
+
+<style>
+	.main-header {
+		background: white;
+		width: 100%;
+		height: 150px;
+		position: absolute;
+	}
+</style>
