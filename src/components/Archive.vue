f293963544d0e118371e8223c254f7fe8d5dde3a
Dkaf <Daniel.j.kafer@gmail.com>
2017-08-22 14:49:19 -0500

    added video filter drop down menu

--- src/components/Archive.vue
+++ src/components/Archive.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="archive">
 	  <h1 class="title">Archive</h1>
+		<select class="video-filter">
+			<option>YouTube</option>
+			<option>Movies</option>
+		</select>
 		<preview></preview>
   </div>
 </template>
@@ -20,5 +24,11 @@ export default {
 <style>
 	.subtitle {
 		font-size: 1.2em;
+	}
+	.video-filter {
+		width: 250px;
+		height: 75px;
+		font-size: 1.4em;
+		text-align: center;
 	}
 </style>
