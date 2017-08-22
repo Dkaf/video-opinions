ce4303c3eb22cd2362de2edf166c02bfc291670a
Dkaf <Daniel.j.kafer@gmail.com>
2017-08-22 14:51:51 -0500

    added more test data

--- src/components/Preview.vue
+++ src/components/Preview.vue
@@ -17,15 +17,30 @@
 				reviews: [
 					{
 						title: 'Birdemic 2',
-						date: '9/9/99'
+						date: '9/9/99',
+						youtube: false,
+						thumbnail: 'url',
+						review: 'adknfkdn ansdfklasdn nkdnfokasdjf'
 					},
 					{
 						title: 'Birdemic',
-						date: '9/8/99'
+						date: '9/8/99',
+						youtube: false
 					},
 					{
 						title: 'Troll 2',
-						date: '9/7/99'
+						date: '9/7/99',
+						youtube: false
+					},
+					{
+						title: 'Cow Chop',
+						date: '9/6/99',
+						youtube: true
+					},
+					{
+						title: 'MFpallytime',
+						date: '9/5/99',
+						youtube: true
 					}
 				]
 			}
