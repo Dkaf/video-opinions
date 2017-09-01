4e3f30664e3307cc3f2afd66a57cf017e8a9dd23
daniel kafer <daniel.j.kafer@gmail.com>
2017-09-01 16:03:49 -0500

    added store to hold state for components

---
+++ src/store.js
@@ -0,0 +1,36 @@
+let store = {
+	state: {
+		selectedVideo: {},
+		reviews: [
+			{
+				title: 'Birdemic 2',
+				date: '9/9/99',
+				youtube: false,
+				thumbnail: 'url',
+				review: 'adknfkdn ansdfklasdn nkdnfokasdjf'
+			},
+			{
+				title: 'Birdemic',
+				date: '9/8/99',
+				youtube: false
+			},
+			{
+				title: 'Troll 2',
+				date: '9/7/99',
+				youtube: false
+			},
+			{
+				title: 'Cow Chop',
+				date: '9/6/99',
+				youtube: true
+			},
+			{
+				title: 'MFpallytime',
+				date: '9/5/99',
+				youtube: true
+			}
+]
+	}
+}
+
+export default store;
