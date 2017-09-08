af8f4b486e9c8c73005af3b2892338b4f414e21a
daniel kafer <daniel.j.kafer@gmail.com>
2017-09-08 13:46:33 -0500

    changed styling and added link to home

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,5 +1,8 @@
 <template>
 	<div class="component main-header">
+		<router-link class="home-link" to='/'>
+			<h1 class="main-title">Dan and Pats' Unsolicited Opinions About...</h1>
+		</router-link>
 		<navigation></navigation>
 	</div>
 </template>
@@ -15,9 +18,18 @@
 <style>
 	.main-header {
 		width: 100%;
-		height: 75px;
+		height: 300px;
 		top: 0;
 		left: 0;
 		overflow-x: hidden;
+		margin-bottom: 30px;
+	}
+	.main-title {
+		font-size: 3em;
+		font-weight: normal;
+	}
+	.home-link {
+		color: black;
+		text-decoration: none;
 	}
 </style>
