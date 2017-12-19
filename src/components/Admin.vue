7ba30a581e8c0697955208ab9ee227d5a3bd7e6c
Dkaf <Daniel.j.kafer@gmail.com>
2017-12-18 21:12:20 -0600

    Added admin components

---
+++ src/components/Admin.vue
@@ -0,0 +1,57 @@
+<template lang="pug">
+  .component.admin
+    form.loginForm(v-on:submit.prevent="login()")
+      fieldset
+        label.formLabel(for='adminPass') Password
+        input.loginInput.pass(type='password' v-model="password")
+        button.formButton(type='submit') Enter
+</template>
+
+<script>
+import {mapActions} from 'vuex'
+import axios from 'axios'
+
+export default {
+  name: 'admin-page',
+  data () {
+    return {
+      password: ''
+    }
+  },
+  methods: {
+    login() {
+        this.$store.dispatch('login', this.password)
+        .then(this.$router.push({name: 'AdminDashboard'}))
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+
+  margin = 0 auto
+  font-size = 1.2em
+
+  .loginForm
+    width: 300px
+    height: 500px
+    margin: margin
+    fieldset
+      margin: margin
+    .formLabel
+      display: block
+      font-size: font-size
+    .loginInput
+      height: 25px
+      margin: 10px
+    .formButton
+      display: block
+      height: 35px
+      width: 90px
+      margin: margin
+      font-size: font-size
+
+</style>
+
+
+
---
+++ src/components/AdminDashboard.vue
@@ -0,0 +1,46 @@
+<template lang="pug">
+  .component.admin-dashboard
+    h1.title Admin dashboard
+    h2.form-title Add Review
+    form(v-on:submit.prevent="addReview({title, type, reviewText})")
+      label(for="title") Title
+      input(type="text" id="title" name="title" v-model="title")
+      fieldset
+        label(for="movie") Movie
+        input(type="radio" id="movie" value="movie" v-model="type")
+        label(for="youtube") YouTube
+        input(type="radio" id="youtube" value="youtube" v-model="type")
+      label(for="new-review-text" name="Text")
+      textarea.new-review-text(v-model="reviewText")
+      button(type="submit") submit
+    h2.form-title Remove Review
+    form
+      fieldset
+        label(for="review-1") Review 1
+        input(type="checkbox" id="review-1" name="review" value="review 1")
+        label(for="review-2") Review 2
+        input(type="checkbox" id="review-2" name="review" value="review 2")
+        label(for="review-3") Review 3
+        input(type="checkbox" id="review-3" name="review" value="review 3")
+    button(type="submit") submit
+</template>
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  name: 'admin-dashboard',
+  data () {
+    return {
+      title: '',
+      reviewText: '',
+      type: ''
+    }
+  },
+  methods: {
+    ...mapActions([
+      'addReview'
+    ]),
+  }
+}
+</script>
+
