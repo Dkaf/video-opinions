ad72a7b940529ca26d87f107707375d16e448cba
Dkaf <Daniel.j.kafer@gmail.com>
2017-12-18 21:16:08 -0600

    added action dispatch to get reviews from server

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,39 @@
-<template>
-  <div id="app">
-    <main-header></main-header>
-    <router-view></router-view>
-  </div>
+<template lang="pug">
+  #app
+    main-header
+    router-view
 </template>
 
 <script>
 import MainHeader from './components/MainHeader'
+import { mapActions } from 'vuex'
+import axios from 'axios'
 export default {
   name: 'app',
-  components: { MainHeader }
+  components: { MainHeader },
+  methods: {
+    ...mapActions([
+      'updateReviewList'
+    ])
+  },
+  created: function () {
+	  this.$store.dispatch('updateReviewList');
+	}
 }
 </script>
 
-<style>
-html {
-  height: 100%;
-}
-#app {
-  height: 100%;
-  width: 100%;
-  font-family: 'Encode Sans Semi Condensed', sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: black;
-  margin-top: 60px;
+<style lang="stylus">
+font = 'Encode Sans Semi Condensed', sans-serif
 
-}
+html, #app
+  height: 100%
+#app
+  width: 100%
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: black
+  margin-top: 60px
+  &,button
+    font-family: font
 </style>
