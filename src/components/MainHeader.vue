77991f2506ce8af595204a74b19ad1bd8cc8773d
Unknown <Daniel.j.kafer@gmail.com>
2017-12-20 09:34:17 -0600

    Removed default home page
    Created new home page and main header components

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,38 @@
+<template>
+  <!-- Configure "view" prop for QLayout -->
+  <q-layout>
+    <q-toolbar slot="header">
+      <!-- opens drawer below
+      <button class="hide-on-drawer-visible" @click="$refs.drawer.open()">
+        <i>menu</i>
+      </button>
+      -->
+      <q-toolbar-title>
+        Title
+      </q-toolbar-title>
+    </q-toolbar>
+
+    <!-- Navigation Tabs
+    <q-tabs slot="navigation">
+      <q-route-tab slot="title" icon="view_quilt" to="/layout/about" replace hide="icon" label="About" />
+      <q-route-tab slot="title" icon="view_day" to="/layout/toolbar" replace hide="icon" label="Toolbar" />
+      <q-route-tab slot="title" icon="view_day" to="/layout/tabs" replace label="Tabs" />
+      <q-route-tab slot="title" icon="input" to="/layout/drawer" replace label="Drawer" />
+    </q-tabs>
+    -->
+    <router-view />
+
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'MainHeader',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="stylus">
+</style>
