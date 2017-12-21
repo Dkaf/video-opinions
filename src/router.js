4996e1eafdf29999c8c47f9c24f46ea044ba0aa6
Dkaf <Daniel.j.kafer@gmail.com>
2017-12-21 14:55:23 -0600

    added admin and review routes

--- src/router.js
+++ src/router.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import HomePage from '@/HomePage.vue'
+import HomePage from '@/HomePage'
+import AdminPage from '@/AdminPage'
+import VideoReview from '@/VideoReview'
 
 Vue.use(VueRouter)
 
@@ -15,19 +17,22 @@ function load (component) {
 */
 
 export default new VueRouter({
-  /*
-   * NOTE! VueRouter "history" mode DOESN'T works for Cordova builds,
-   * it is only to be used only for websites.
-   *
-   * If you decide to go with "history" mode, please also open /config/index.js
-   * and set "build.publicPath" to something other than an empty string.
-   * Example: '/' instead of current ''
-   *
-   * If switching back to default "hash" mode, don't forget to set the
-   * build publicPath back to '' so Cordova builds work again.
-   */
-
   routes: [
-    { path: '/', component: HomePage }
+    {
+      path: '/',
+      name: 'Home',
+      component: HomePage
+    },
+    {
+      path: '/admin',
+      name: 'Admin',
+      component: AdminPage
+    },
+    {
+      path: '/review/:name',
+      name: 'Review',
+      component: VideoReview,
+      props: true
+    }
   ]
 })
