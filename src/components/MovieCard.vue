07d03f6f8140c4ed76a522fb926d0abf10bd5508
Dkaf <Daniel.j.kafer@gmail.com>
2017-12-21 14:55:56 -0600

    added components to display basic movie info and reviews

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div v-for="review in reviews" v-bind:key="review.id" >
+      <router-link :to="{name: 'Review', params: {name: review.title}}">
+        <h2>{{review.title}}</h2>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MovieCard',
+  props: {
+    reviews: Array
+  },
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="stylus">
+</style>
---
+++ src/components/VideoReview.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{ this.selected.title }}</h1>
+    <span>{{ name }}</span>
+    <p>{{ this.selected.review }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VideoReview',
+  props: {
+    name: String
+  },
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="stylus">
+</style>
