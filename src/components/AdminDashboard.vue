c3c4c7ba2bbc6f72f9ffcb7e7307e0e1f8d4e14b
Dkaf <Daniel.j.kafer@gmail.com>
2017-12-21 20:30:07 -0600

    Added admin dashboard component

---
+++ src/components/AdminDashboard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="layout-padding">
+     <change-password />
+  </div>
+</template>
+
+<script>
+import ChangePassword from './ChangePassword'
+export default {
+  name: 'AdminDashboard',
+  components: { ChangePassword },
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="stylus">
+</style>
