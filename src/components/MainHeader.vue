f77b9bc84df59df7d11d4198025bdbb634d9edf2
Dkaf <Daniel.j.kafer@gmail.com>
2017-12-21 20:32:45 -0600

    removed extra comments and properly configured layout and view

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,25 +1,22 @@
 <template>
   <!-- Configure "view" prop for QLayout -->
-  <q-layout>
+  <q-layout view="hHh lpr fff">
     <q-toolbar slot="header">
-      <!-- opens drawer below
-      <button class="hide-on-drawer-visible" @click="$refs.drawer.open()">
-        <i>menu</i>
-      </button>
-      -->
+
       <q-toolbar-title>
-        Title
+        Pats' Unsolicited Opinions
       </q-toolbar-title>
     </q-toolbar>
 
-    <q-tabs slot="navigation">
-      <q-route-tab slot="title" to="/movies" replace hide="icon" label="Movies" />
-      <q-route-tab slot="title" to="/games" replace hide="icon" label="Games" />
-      <q-route-tab slot="title" to="/youtube" replace label="YouTube" />
-      <q-route-tab slot="title" to="/about" replace label="Me" />
+    <q-tabs slot="navigation" align="center">
+      <q-route-tab slot="title" to="/" exact hide="icon" label="Home" />
+      <q-route-tab slot="title" to="/movies" exact hide="icon" label="Movies" />
+      <q-route-tab slot="title" to="/games" exact hide="icon" label="Games" />
+      <q-route-tab slot="title" to="/youtube" exact label="YouTube" />
+      <q-route-tab slot="title" to="/about" exact label="Me" />
     </q-tabs>
-   
-    <router-view />
+
+    <router-view class="layout-view" ></router-view>
 
   </q-layout>
 </template>
