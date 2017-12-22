7c4671e2e4330e3b817e66b56f82a2fb2d629427
Dkaf <Daniel.j.kafer@gmail.com>
2017-12-21 20:36:23 -0600

    Now gets selected review and renders content correctly

--- src/components/VideoReview.vue
+++ src/components/VideoReview.vue
@@ -1,19 +1,25 @@
 <template>
-  <div>
-    <h1>{{ this.selected.title }}</h1>
-    <span>{{ name }}</span>
-    <p>{{ this.selected.review }}</p>
+  <div class="layout-padding">
+    <h1>{{ name }}</h1>
+    <span>{{ selected.date }}</span>
+    <q-video v-if="selected.type === 'youtube'" src="selected.media" alt="YouTube Player" />
+    <img v-if="selected.type === 'movie'" class="responsive" src="selected.media" alt="Movie Poster" />
+    <p>{{ selected.review }}</p>
   </div>
 </template>
 
 <script>
+import { QVideo } from 'quasar'
 export default {
   name: 'VideoReview',
   props: {
     name: String
   },
-  data () {
-    return {}
+  components: { QVideo },
+  computed: {
+    selected: function () {
+      return this.$store.getters.getReview(this.name)
+    }
   }
 }
 </script>
