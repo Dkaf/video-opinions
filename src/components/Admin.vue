746d2aeb763667cd0564c252634a6952b09f46f6
Unknown <Daniel.j.kafer@gmail.com>
2018-02-19 08:53:29 -0600

    Added change password form
    Added form to change password, added new data and login method

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -5,6 +5,15 @@
         label.formLabel(for='adminPass') Password
         input.loginInput.pass(type='password' v-model="password")
         button.formButton(type='submit') Enter
+    h2 Change Password
+    form.passChange(v-on:submit.prevent="")
+      label.formLabel(for='oldPass') Old Password
+      input.passChangeInput.oldPass(type='password' v-model="oldPass")
+      label.formLabel(for='newPass') New Password
+      input.passChangeInput.newPass(type='password' v-model="newPass")
+      label.formLabel(for='confirmPass') Confirm Password
+      input.passChangeInput.confirmPass(type='password' v-model="confirmPass")
+      input(type='submit')
 </template>
 
 <script>
@@ -15,13 +24,23 @@ export default {
   name: 'admin-page',
   data () {
     return {
-      password: ''
+      password: '',
+      oldPass: '',
+      newPass: '',
+      confirmPass: ''
     }
   },
   methods: {
     login() {
         this.$store.dispatch('login', this.password)
-        .then(this.$router.push({name: 'AdminDashboard'}))
+        .then( () => {
+          (this.$store.state.loggedIn) ? this.$router.push({name: 'AdminDashboard'}) : console.log('login error')
+        })
+    },
+    changePassword() {
+      (this.newPass === this.confirmPass) ?
+        this.$store.dispatch('changePass', {newPass: this.newPass, oldPass: this.oldPass })
+        : console.log('passwords do not match')
     }
   }
 }
@@ -36,20 +55,21 @@ export default {
     width: 300px
     height: 500px
     margin: margin
-    fieldset
-      margin: margin
-    .formLabel
-      display: block
-      font-size: font-size
-    .loginInput
-      height: 25px
-      margin: 10px
-    .formButton
-      display: block
-      height: 35px
-      width: 90px
-      margin: margin
-      font-size: font-size
+  fieldset
+    margin: margin
+    border: none
+  .formLabel
+    display: block
+    font-size: font-size
+  .loginInput
+    height: 25px
+    margin: 10px
+  .formButton
+    display: block
+    height: 35px
+    width: 90px
+    margin: margin
+    font-size: font-size
 
 </style>
 
