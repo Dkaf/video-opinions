8c415eb07d5156fd762fe97f947fa9d32520b47b
Unknown <Daniel.j.kafer@gmail.com>
2018-02-19 08:58:38 -0600

    Added computed function to check review type

--- src/components/Review.vue
+++ src/components/Review.vue
@@ -1,14 +1,24 @@
 <template lang="pug">
 	.component.article
+		movie(v-if="selected.type === 'movie'")
+		youtube(v-if="selected.type === 'youtube'")
 		h1.title {{ this.selected.title }}
 		span.date {{ this.selected.date }}
 		p.review-content {{ this.selected.review }}
 </template>
 
 <script>
+import Youtube from './Youtube'
+import Movie from './Movie'
 export default {
   name: 'review',
-	props: ['selected']
+	props: ['name'],
+	components: { Movie, Youtube },
+	computed: {
+		selected: function() {
+			return this.$store.getters.getReview(this.name)
+		}
+	}
 }
 </script>
 <style>
